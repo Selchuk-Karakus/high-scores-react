4058e54ecad501919bf08f9ada92529a1b541af5
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-03-31 19:46:53 +0100

    Add level 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import React, { useState } from "react";
 import "./App.css";
 import Data from "./data/scores";
 import HighScoresTable from "./HighScoresTable";
 
 function App() {
+  const [isOrdered, setIsOrdered] = useState(true);
+
+  let alphabeticalCountryOrder = Data.sort((a, b) => {
+    return a.name < b.name ? -1 : 1;
+  });
+
+  alphabeticalCountryOrder.map((country) => {
+    return country.scores.sort((personA, personB) => {
+      return isOrdered
+        ? personA.s > personB.s
+          ? -1
+          : 1
+        : personA.s < personB.s
+        ? -1
+        : 1;
+    });
+  });
+
+  const handleClick = () => {
+    setIsOrdered(!isOrdered);
+  };
+
   return (
-    <>
-      <HighScoresTable scores={Data} />
-    </>
+    <div className="main-container">
+      <button className="button" onClick={handleClick}>Reverse Player Score Order</button>
+      <HighScoresTable scores={alphabeticalCountryOrder} />
+    </div>
   );
 }
 
--- src/HighScoresTable.js
+++ src/HighScoresTable.js
@@ -2,14 +2,10 @@ import React from "react";
 import PlayerScore from "./PlayerScore";
 
 const HighScoresTable = ({ scores }) => {
-  let sorted = scores.sort((a, b) => {
-    return a.name < b.name ? -1 : 1;
-  });
-
   return (
-    <div className="main-container">
+    <div>
       <h1 className="main-header">High Scores per Country</h1>
-      {sorted.map(({ name, scores }, ind) => {
+      {scores.map(({ name, scores }, ind) => {
         return (
           <div key={ind} className="table-wrapper">
             <h3 className="country-name">HIGH SCORES: {name}</h3>
